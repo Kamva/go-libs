0c668a8b851677dd5d6b807d2584bbbb81427e32
Behzad Shabani <behzad.shabani@gmail.com>
2018-09-18 16:42:39 +0430

    Add error bag utility

---
+++ utils/error_bag.go
@@ -0,0 +1,20 @@
+package utils
+
+import "github.com/kamva/go-libs/contracts"
+
+type ErrorBag struct {
+	errors   map[string][]string
+	taggable contracts.Taggable
+}
+
+func (b ErrorBag) Append(field string, value string) {
+	fieldName := b.taggable.GetTag(field, "json")
+	b.errors[fieldName] = append(b.errors[fieldName], value)
+}
+func (b ErrorBag) GetErrors() interface{} {
+	return b.errors
+}
+
+func NewErrorBag(taggable contracts.Taggable) ErrorBag {
+	return ErrorBag{errors: make(map[string][]string), taggable: taggable}
+}

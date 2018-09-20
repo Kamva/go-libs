b2271e4421acfb9fac52b82be0a148f4dcc6072f
Behzad Shabani <behzad.shabani@gmail.com>
2018-09-20 13:39:24 +0430

    Update base types

--- contracts/base_types.go
+++ contracts/base_types.go
@@ -1,10 +1,30 @@
 package contracts
 
-import "reflect"
+import (
+	"reflect"
+	"github.com/globalsign/mgo/bson"
+)
 
-type BaseRequest struct {}
+type BaseTaggable struct {}
 
-func (r BaseRequest) GetTag(field string, tag string) string {
+func (r BaseTaggable) GetTag(field string, tag string) string {
 	taggedField, _ := reflect.TypeOf(r).FieldByName(field)
 	return taggedField.Tag.Get(tag)
+}
+
+type BaseRequest struct{
+	BaseTaggable
+}
+
+type BaseScheme struct {
+	BaseTaggable
+	Id bson.ObjectId `bson:"_id" json:"id"`
+}
+
+func (a BaseScheme) GetId() bson.ObjectId {
+	return a.Id
+}
+
+func (a *BaseScheme) SetId(id bson.ObjectId) {
+	a.Id = id
 }

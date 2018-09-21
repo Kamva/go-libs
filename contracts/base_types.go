0426b73f373a64f49ce236c623fa5dc7fd657e32
Behzad Shabani <behzad.shabani@gmail.com>
2018-09-21 12:52:41 +0430

    Fix issue with reflect in GetTag

--- contracts/base_types.go
+++ contracts/base_types.go
@@ -8,7 +8,7 @@ import (
 type BaseTaggable struct {}
 
 func (r BaseTaggable) GetTag(caller interface{}, field string, tag string) string {
-	taggedField, _ := reflect.TypeOf(caller).FieldByName(field)
+	taggedField, _ := reflect.TypeOf(caller).Elem().FieldByName(field)
 	return taggedField.Tag.Get(tag)
 }
 

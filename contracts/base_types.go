db6ff9aaef67bb524c7991b464520736efccb371
Behzad Shabani <behzad.shabani@gmail.com>
2018-09-22 12:38:36 +0330

    Considering base scheme as bad idea

--- contracts/base_types.go
+++ contracts/base_types.go
@@ -2,29 +2,15 @@ package contracts
 
 import (
 	"reflect"
-	"github.com/globalsign/mgo/bson"
 )
 
-type BaseTaggable struct {}
+type BaseTaggable struct{}
 
 func (r BaseTaggable) GetTag(caller interface{}, field string, tag string) string {
 	taggedField, _ := reflect.TypeOf(caller).Elem().FieldByName(field)
 	return taggedField.Tag.Get(tag)
 }
 
-type BaseRequest struct{
+type BaseRequest struct {
 	BaseTaggable
-}
-
-type BaseScheme struct {
-	BaseTaggable
-	Id bson.ObjectId `bson:"_id" json:"id"`
-}
-
-func (a BaseScheme) GetId() bson.ObjectId {
-	return a.Id
-}
-
-func (a *BaseScheme) SetId(id bson.ObjectId) {
-	a.Id = id
 }

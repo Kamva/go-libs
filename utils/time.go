89a8732f4556c5e2bdc8cdbcad5caf5013246164
Behzad Shabani <behzad.shabani@gmail.com>
2018-09-29 16:56:37 +0330

    Optimize DateTime

--- utils/time.go
+++ utils/time.go
@@ -3,18 +3,31 @@ package utils
 import "time"
 
 type DateTime struct {
-	time time.Time
+	timezone *time.Location
+	time     time.Time
 }
 
 func NewDateTime() *DateTime {
 	loc, _ := time.LoadLocation("Asia/Tehran")
-	return &DateTime{time: time.Now().In(loc)}
+
+	return &DateTime{
+		time: time.Now().In(loc),
+		timezone: loc,
+	}
 }
 
 func (dt *DateTime) ToW3cString() string {
+	dt.renewTime()
+
 	return dt.time.Format("2006-01-02T15:04:05-07:00")
 }
 
 func (dt *DateTime) Timestamp() int64 {
+	dt.renewTime()
+
 	return dt.time.Unix()
+}
+
+func (dt *DateTime) renewTime() {
+	dt.time = time.Now().In(dt.timezone)
 }
